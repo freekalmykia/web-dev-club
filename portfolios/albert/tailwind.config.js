078a58c60e05cf5e3cd8ddb15f8451a21012ceb7
AlSharp <elistaphilly@gmail.com>
2023-06-01 23:17:40 -0400

    albert: portfolio first commit

---
+++ portfolios/albert/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'cherry-bomb-one': 'Cherry Bomb One'
+      },
+      colors: {
+        'body': '#18161f'
+      }
+    },
+  },
+  plugins: [],
+}
+
