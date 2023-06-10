b98e074e933867ec1aab16a1abd8a2582113670c
AlSharp <elistaphilly@gmail.com>
2023-06-10 13:12:25 -0400

    open and close menu

--- portfolios/albert/src/main.js
+++ portfolios/albert/src/main.js
@@ -4,11 +4,23 @@ const menuButton = document.getElementById('menu-button');
 
 console.log('menuButton: ', menuButton);
 
+menuButton.addEventListener('click', handleMenuButtonClick);
+
+const closeButton = document.getElementById('menu-overlay').querySelector('div > svg');
+
+console.log('closeButton: ', closeButton);
+
+const handleCloseButtonClick = () => {
+  console.log('close button clicked');
+  const menu = document.getElementById('menu-overlay');
+  menu.classList.add('hidden');
+}
+
+closeButton.addEventListener('click', handleCloseButtonClick);
+
 function handleMenuButtonClick() {
   console.log('menu button clicked!');
   const menu = document.getElementById('menu-overlay');
   menu.classList.remove('hidden');
 }
 
-
-menuButton.addEventListener('click', handleMenuButtonClick);
