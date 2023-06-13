8dac3a9e6197e62ffaedaf4aba84dc360e34e3e3
HelenIva <heleniva02@gmail.com>
2023-06-12 22:18:19 -0400

    portfolios: add  test

---
+++ portfolios/elena/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+
+      }
+    },
+
+  },
+  plugins: [],
+}
+
