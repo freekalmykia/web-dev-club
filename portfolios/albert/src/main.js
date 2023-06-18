b98b0db10881d8928a6a0287ce784236fa18b1c5
AlSharp <elistaphilly@gmail.com>
2023-06-18 01:16:53 -0400

    add porjects

--- portfolios/albert/src/main.js
+++ portfolios/albert/src/main.js
@@ -12,19 +12,9 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 const menuButton = document.getElementById('menu-button');
 
-console.log('menuButton: ', menuButton);
-
 menuButton.addEventListener('click', handleMenuButtonClick);
 
 const closeButton = document.getElementById('menu-overlay').querySelector('div > svg');
-
-console.log('closeButton: ', closeButton);
-
-const handleCloseButtonClick = () => {
-  console.log('close button clicked');
-  const menu = document.getElementById('menu-overlay');
-  menu.classList.add('hidden');
-}
 
 closeButton.addEventListener('click', handleCloseButtonClick);
 
@@ -32,5 +22,11 @@ function handleMenuButtonClick() {
   console.log('menu button clicked!');
   const menu = document.getElementById('menu-overlay');
   menu.classList.remove('hidden');
+}
+
+function handleCloseButtonClick() {
+  console.log('close button clicked');
+  const menu = document.getElementById('menu-overlay');
+  menu.classList.add('hidden');
 }
 
