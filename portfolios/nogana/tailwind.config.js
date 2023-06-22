1f45988f85f816eee994d95b7159f4add2383906
Nogana <nbdzhambinova@gmail.com>
2023-06-22 01:01:55 -0400

    add my portfolio

---
+++ portfolios/nogana/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        "black-theme": "#17171F",
+        "blue-button": "#4163b8"
+      },
+      fontFamily: {
+        'kaushan-script': 'Kaushan Script'
+      }
+    },
+  },
+  plugins: [],
+}
+
